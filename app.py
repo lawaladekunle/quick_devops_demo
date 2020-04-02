ad070d54a9d596895cb579bc06408263cc05beb2
Lawal Adekunle <lawaladekunle@gmail.com>
2020-04-02 11:11:14 +0100

    Added a demo page
    Added a demo page to the flask app.

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/demo")
+def demo_page():
+    return "Welcome. This is a Demo."
+
 if __name__ == "__main__":
     app.run()
